dfc4fe9c0008f71b1b8371a6b4c2834b707a4a78
valehaedo <valeriahaedo@gmail.com>
2020-08-05 19:38:17 +0200

    end of app

--- statemanagment/src/App.js
+++ statemanagment/src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import './App.css';
 import MovieList from './MovieList';
+import Nav from './Nav';
+import AddMovie from './AddMovie';
+import { MovieProvider } from './MovieContext';
+
 
 function App() {
+
   return (
-    <div className="App">
-      <MovieList />
-    </div>
+    <MovieProvider>
+      <div className="App">
+        <Nav />
+        <AddMovie/>
+        <MovieList />
+      </div>
+    </MovieProvider>
   );
 }
 
